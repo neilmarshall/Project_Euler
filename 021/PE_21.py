d230f27db5ddf5509f281fa416efe62aec67909e
Neil <neil.marshall@dunelm.org.uk>
2018-09-27 08:59:17 +0100

    Update PE_21.py

--- 021/PE_21.py
+++ 021/PE_21.py
@@ -10,16 +10,11 @@ and 110; therefore d(220) = 284. The proper divisors of 284 are 1, 2, 4, 71 and
 142; so d(284) = 220.
 
 Evaluate the sum of all the amicable numbers under 10000.
-
 """
 
 def d(n):
     """Return the sum of proper divisors of n"""
-    s = 0
-    for p in range(1, n // 2 + 1):
-        if n % p == 0:
-            s += p
-    return s
+    return sum(p for p in range(1, n // 2 + 1) if n % p == 0)
 
 
 def PE_21(limit):
