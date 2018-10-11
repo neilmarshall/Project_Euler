cbb53c20de310435f487c115b371598063ac84bf
Neil <neil.marshall@dunelm.org.uk>
2018-10-11 08:45:37 +0100

    Replace deque with priority_queue in pythagorean_triples.cpp

--- utilities/src/pythagorean_triples.cpp
+++ utilities/src/pythagorean_triples.cpp
@@ -2,13 +2,13 @@
 
 template <typename T>
 PythagoreanTriples::PythagoreanTripleGenerator<T>::PythagoreanTripleGenerator () {
-    triples.push_back(std::make_tuple(3, 4, 5));
+    triples.push(std::make_tuple(3, 4, 5));
 }
 
 template <typename T>
 std::tuple<T, T, T> PythagoreanTriples::PythagoreanTripleGenerator<T>::GetNextTriple() {
-    auto next_triple = triples.front();
-    triples.pop_front();
+    auto next_triple = triples.top();
+    triples.pop();
     T a, b, c;
     std::tie(a, b, c) = next_triple;
     add_new_triples(a, b, c);
@@ -24,21 +24,21 @@ void PythagoreanTriples::PythagoreanTripleGenerator<T>::add_new_triples(T a, T b
     elements[1] = 2 * a - b + 2 * c;
     elements[2] = 2 * a - 2 * b + 3 * c;
     std::sort(elements.begin(), elements.end());
-    triples.push_back(std::make_tuple(elements[0], elements[1], elements[2]));
+    triples.push(std::make_tuple(elements[0], elements[1], elements[2]));
 
     // matrix multiplication using [[1, 2, 2], [2, 1, 2], [2, 2, 3]]
     elements[0] = a + 2 * b + 2 * c;
     elements[1] = 2 * a + b + 2 * c;
     elements[2] = 2 * a + 2 * b + 3 * c;
     std::sort(elements.begin(), elements.end());
-    triples.push_back(std::make_tuple(elements[0], elements[1], elements[2]));
+    triples.push(std::make_tuple(elements[0], elements[1], elements[2]));
 
     // matrix multiplication using [[-1, 2, 2], [-2, 1, 2], [-2, 2, 3]]
     elements[0] = -a + 2 * b + 2 * c;
     elements[1] = -2 * a + b + 2 * c;
     elements[2] = -2 * a + 2 * b + 3 * c;
     std::sort(elements.begin(), elements.end());
-    triples.push_back(std::make_tuple(elements[0], elements[1], elements[2]));
+    triples.push(std::make_tuple(elements[0], elements[1], elements[2]));
 }
 
 template class PythagoreanTriples::PythagoreanTripleGenerator<char>;
