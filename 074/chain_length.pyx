ed2195a9cb63588e61c9a9ea4a2acea436bcabaf
neilmarshall <neil.marshall@dunelm.org.uk>
2018-10-15 22:30:56 +0100

    More highly optimized cython

--- 074/chain_length.pyx
+++ 074/chain_length.pyx
@@ -1,15 +1,18 @@
 cpdef int digit_factorial_sum(int n):
-    cdef dict factorial = {'0': 1, '1': 1, '2': 2, '3': 6, '4': 24, '5': 120,
-                           '6': 720, '7': 5040, '8': 40320, '9': 362880}
+    cdef dict factorial = {0: 1, 1: 1, 2: 2, 3: 6, 4: 24, 5: 120, 6: 720, 7: 5040, 8: 40320, 9: 362880}
     cdef int t = 0
-    for c in str(n):
+    cdef int c
+    while n >= 10:
+        c = n % 10
         t += factorial[c]
+        n //= 10
+    t += factorial[n]
     return t
 
 
 cpdef int chain_length(int n):
     cdef int chain = 1
-    cdef set seen = set((n,))
+    cdef set seen = {n}
     while True:
         n = digit_factorial_sum(n)
         if n not in seen:
