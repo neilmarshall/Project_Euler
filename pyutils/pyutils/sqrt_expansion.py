6bfd57d92829af903a34fdedebf83edcf7db38df
neilmarshall <neil.marshall@dunelm.org.uk>
2018-10-22 21:55:56 +0100

    Add sqrt_expansion module and associated test suite

---
+++ pyutils/pyutils/sqrt_expansion.py
@@ -0,0 +1,38 @@
+from fractions import Fraction
+
+class SqrtExpansion():
+    """Generates square root expansion of an irrational number"""
+
+    def __init__(self, n, iterable=None):
+        if iterable is None:
+            self.root, self.key = self.get_root_and_key(n)
+        else:
+            self.root = n
+            self.key = iterable
+
+    def __str__(self):
+        return "[{0}; ({1})]".format(self.root, ', '.join(map(str, self.key)))
+
+    def get_root_and_key(self, n):
+        """Return square root expansion root and key for square root of n"""
+        root = int(n**0.5)
+        a_0 = epsilon_0 = int(n**0.5)
+        gamma_0 = n - epsilon_0**2
+        a, epsilon, gamma = a_0, epsilon_0, gamma_0
+        expansion = []
+        while (True):
+            a = (int(n**0.5) + epsilon) // gamma
+            epsilon = a * gamma - epsilon
+            gamma = (n - epsilon**2) // gamma
+            expansion.append(a)
+            if (epsilon == epsilon_0 and gamma == gamma_0):
+                break
+        return root, tuple(expansion)
+
+    def get_nth_fraction(self, n):
+        """Return nth fractional estimate of square root of n"""
+        expansion = [self.key[i % len(self.key)] for i in range(n)]
+        numerator, denominator = 0, 1
+        for exp in reversed(expansion[:n - 1]):
+            numerator, denominator = denominator, numerator + denominator * exp
+        return Fraction(self.root, 1) + Fraction(numerator, denominator)
---
+++ pyutils/tests/test_sqrt_expansion.py
@@ -0,0 +1,55 @@
+import unittest
+from fractions import Fraction
+
+from pyutils.sqrt_expansion import SqrtExpansion
+
+class TestSqrtExpansion(unittest.TestCase):
+
+    def test_generates_root_for_expansion_of_2_when_given_key_and_root(self):
+        expansion = SqrtExpansion(1, (2,))
+        self.assertEqual(expansion.root, 1)
+
+    def test_generates_key_for_expansion_of_2_when_given_key_and_root(self):
+        expansion = SqrtExpansion(1, (2,))
+        self.assertEqual(expansion.key, (2,))
+
+    def test_generates_root_for_expansion_of_2_when_initialised_with_2(self):
+        expansion = SqrtExpansion(2)
+        self.assertEqual(expansion.root, 1)
+
+    def test_generates_key_for_expansion_of_2_when_initialised_with_2(self):
+        expansion = SqrtExpansion(2)
+        self.assertEqual(expansion.key, (2,))
+
+    def test_generates_root_for_expansion_of_13_when_initialised_with_13(self):
+        expansion = SqrtExpansion(13)
+        self.assertEqual(expansion.root, 3)
+
+    def test_generates_key_for_expansion_of_13_when_initialised_with_13(self):
+        expansion = SqrtExpansion(13)
+        self.assertEqual(expansion.key, (1, 1, 1, 1, 6))
+
+    def test_generates_first_four_fractions_for_expansion_of_2(self):
+        expansion = SqrtExpansion(2)
+        actual = tuple((expansion.get_nth_fraction(1), expansion.get_nth_fraction(2),
+                        expansion.get_nth_fraction(3), expansion.get_nth_fraction(4)))
+        expected = tuple((Fraction(1, 1), Fraction(3, 2),
+                          Fraction(7, 5), Fraction(17, 12)))
+        self.assertEqual(actual, expected)
+
+    def test_generates_first_five_fractions_for_expansion_of_e(self):
+        expansion = SqrtExpansion(2, (1, 2, 1, 1, 4))
+        actual = tuple((expansion.get_nth_fraction(1), expansion.get_nth_fraction(2),
+                        expansion.get_nth_fraction(3), expansion.get_nth_fraction(4),
+                        expansion.get_nth_fraction(5)))
+        expected = tuple((Fraction(2, 1), Fraction(3, 1), Fraction(8, 3),
+                          Fraction(11, 4), Fraction(19, 7)))
+        self.assertEqual(actual, expected)
+
+    def test_generates_eighth_fraction_for_expansion_of_23(self):
+        expansion = SqrtExpansion(23)
+        self.assertEqual(expansion.get_nth_fraction(8), Fraction(1151, 240))
+
+    def test_string_representation_of_expansion_of_23(self):
+        expansion = SqrtExpansion(23)
+        self.assertEqual(str(expansion), "[4; (1, 3, 1, 8)]")
