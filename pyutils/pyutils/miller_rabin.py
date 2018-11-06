86b6fefed47192c7f2a867823968a901768a17eb
neilmarshall <neil.marshall@dunelm.org.uk>
2018-11-06 13:15:06 +0000

    refactor (pyutils): Convert Miller-Rabin function to a class
    The Miller-Rabin algorithm is slow compared to trial division for small
    n, so it is necessary for performance reasons to first attempt trial
    division versus an intial list of primes.
    
    To avoid repeatedly calculating these initial primes (and to avoid
    hard-coding their values in the source code) a class is implemented,
    with calculation of initial primes delegated to the __init__ method.
    
    Primality of a given number can then be checked by calling the class'
    `is_prime` method.

--- pyutils/pyutils/miller_rabin.py
+++ pyutils/pyutils/miller_rabin.py
@@ -1,58 +1,76 @@
 from math import floor, log
 
-def factorise_powers_of_two(n):
-    s, m = 0, n - 1
-    while m % 2 == 0:
-        s += 1
-        m >>= 1
-    d = n >> s
-    return s, d
+from pyutils.primes import get_primes_up_to_n
 
-
-def miller_rabin_test(n):
+class MillerRabin():
     """
     Miller-Rabin primality test
 
-    Deterministic implementation of Miller-Rabin primality test
+    Deterministic implementation of Miller-Rabin primality test, based on
     https://en.wikipedia.org/wiki/Miller%E2%80%93Rabin_primality_test
+
+    Class loads primes below 5,000. For given n, assuming n odd (returning False
+    if n is not 2 otherwise), trial division is first attempted versus these
+    initial primes.
+
+    If trial division fails to identify primality, the full Miller-Rabin algorithm
+    is employed, with witnesses chosen based on the value of n.
+
+    Empirically, the MillerRabin class appears to out-perform trial division for
+    primes larger than 1,000,000 (trial division tested all primes up to 1,000,000
+    in just over 6s; Miller-Rabin achieved the same in just under 6s).
     """
-    # discount even numbers
-    if n <= 2 or n % 2 == 0:
-        return n == 2
+    def __init__(self):
+        self.known = get_primes_up_to_n(5000)
 
-    # perform trial division based on initial primes
-    initial_primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47,
-                      53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
-    if n in initial_primes:
+    def is_prime(self, n):
+        """
+        Return primality of n
+        
+        Uses trial division for small n and Miller-Rabin for larger values.
+        """
+        # discount even numbers
+        if n <= 2 or n % 2 == 0:
+            return n == 2
+
+        # perform trial division based on initial primes
+        for p in self.known:
+            if n % p == 0:
+                return n == p
+
+        # test compositeness using Miller-Rabin algorithm, with suitable witnesses
+        if n < 2047:
+            witnesses = [2]
+        elif n < 1373653:
+            witnesses = [2, 3]
+        elif n < 9080191:
+            witnesses = [31, 73]
+        elif n < 25326001:
+            witnesses = [2, 3, 5]
+        elif n < 3215031751:
+            witnesses = [2, 3, 5, 7]
+        elif n < 4759123141:
+            witnesses = [2, 7, 61]
+        elif n < 1122004669633:
+            witnesses = [2, 13, 23, 1662803]
+        elif n < 2152302898747:
+            witnesses = [2, 3, 5, 7, 11]
+        elif n < 3474749660383:
+            witnesses = [2, 3, 5, 7, 11, 13]
+        elif n < 341550071728321:
+            witnesses = [2, 3, 5, 7, 11, 13, 17]
+
+        s, d = self._factorise_powers_of_two(n)
+        for a in witnesses:
+            if pow(a, d, n) != 1:
+                if all(pow(a, pow(2, r) * d, n) != n - 1 for r in range(s)):
+                    return False
         return True
-    if any(n % p == 0 and n < p for p in initial_primes):
-        return False
 
-    # test compositeness using Miller-Rabin algorithm, with suitable witnesses
-    if n < 2047:
-        witnesses = [2]
-    elif n < 1373653:
-        witnesses = [2, 3]
-    elif n < 9080191:
-        witnesses = [31, 73]
-    elif n < 25326001:
-        witnesses = [2, 3, 5]
-    elif n < 3215031751:
-        witnesses = [2, 3, 5, 7]
-    elif n < 4759123141:
-        witnesses = [2, 7, 61]
-    elif n < 1122004669633:
-        witnesses = [2, 13, 23, 1662803]
-    elif n < 2152302898747:
-        witnesses = [2, 3, 5, 7, 11]
-    elif n < 3474749660383:
-        witnesses = [2, 3, 5, 7, 11, 13]
-    elif n < 341550071728321:
-        witnesses = [2, 3, 5, 7, 11, 13, 17]
-
-    s, d = factorise_powers_of_two(n)
-    for a in witnesses:
-        if pow(a, d, n) != 1:
-            if all(pow(a, pow(2, r) * d, n) != n - 1 for r in range(s)):
-                return False
-    return True
+    def _factorise_powers_of_two(self, n):
+        s, m = 0, n - 1
+        while m % 2 == 0:
+            s += 1
+            m >>= 1
+        d = n >> s
+        return s, d
--- pyutils/tests/test_miller_rabin.py
+++ pyutils/tests/test_miller_rabin.py
@@ -1,16 +1,23 @@
 import unittest
 
-from pyutils.miller_rabin import miller_rabin_test
-from pyutils.primes import get_primes_under_n
+from pyutils.miller_rabin import MillerRabin
 
 class TestMillerRabin(unittest.TestCase):
 
+    @classmethod
+    def setUpClass(cls):
+        cls.miller_rabin = MillerRabin()
+
     def test_miller_rabin_produces_primes_under_100(self):
-        miller_rabin_output = [n for n in range(100) if miller_rabin_test(n)]
+        miller_rabin_output = [n for n in range(100) if self.miller_rabin.is_prime(n)]
         primes_under_100 = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, \
                             47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
         self.assertEqual(miller_rabin_output, primes_under_100)
 
     def test_miller_rabin_produces_sum_of_primes_under_1000(self):
-        miller_rabin_output = sum(n for n in range(1000) if miller_rabin_test(n))
-        self.assertEqual(miller_rabin_output, sum(get_primes_under_n(1000)))
+        miller_rabin_output = sum(n for n in range(1000) if self.miller_rabin.is_prime(n))
+        self.assertEqual(miller_rabin_output, 76127)
+
+    def test_miller_rabin_produces_sum_of_pries_between_100000_and_120000(self):
+        miller_rabin_output = sum(n for n in range(100000, 120000) if self.miller_rabin.is_prime(n))
+        self.assertEqual(miller_rabin_output, 187964655)
