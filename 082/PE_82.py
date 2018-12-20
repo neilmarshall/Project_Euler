2e4ff37aaccb462e9ab40bc557677e618434c1a0
neilmarshall <neil.marshall@dunelm.org.uk>
2018-12-20 19:48:05 +0000

    test (082): Add missing unit tests

--- 082/PE_82.py
+++ 082/PE_82.py
@@ -1,3 +1,12 @@
+#! /usr/bin/env python3.7
+"""
+Find the minimal path sum, in matrix.txt, a 31K text file containing a 80 by 80 
+matrix, from the left column to the right column.
+
+Solution: 260324
+"""
+import unittest
+
 def solve(M, verbose=False):
     """
     >>> M = [[234,103,18],[342,965,150],[746,422,111]]
@@ -28,8 +37,21 @@ def solve(M, verbose=False):
     return min(reduce_to_distances(rows_to_columns(M)))
 
 
-if __name__ == '__main__':
-    import doctest; doctest.testmod()
-    with open("p082_matrix.txt") as f:
-        print(solve([list(map(int, line.split(','))) for line in f]))
+class TestSolve(unittest.TestCase):
+    def test_3_by_3_matrix(self):
+        M = [[234,103,18],[342,965,150],[746,422,111]]
+        self.assertEqual(solve(M), 355)
+    
+    def test_5_by_5_matrix(self):
+        M = [[131,673,234,103,18],[201,96,342,965,150],[630,803,746,422,111],\
+             [537,699,497,121,956],[805,732,524,37,331]]
+        self.assertEqual(solve(M), 994)
 
+    def test_80_by_80_matrix(self):
+        with open("p082_matrix.txt") as f:
+            M = [list(map(int, line.split(','))) for line in f]
+        self.assertEqual(solve(M), 260324)
+
+
+if __name__ == '__main__':
+    unittest.main()
