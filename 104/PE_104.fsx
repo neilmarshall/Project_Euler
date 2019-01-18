811ba126db7b97f5cd72d8424427aa2ffaaf52f6
Neil <neil.marshall@dunelm.org.uk>
2019-01-18 14:01:22 +0000

    Create PE_104.fsx

---
+++ 104/PE_104.fsx
@@ -0,0 +1,38 @@
+#! /usr/bin/env fsharpi
+
+// The Fibonacci sequence is defined by the recurrence relation:
+//     F(n) = F(n − 1) + F(n − 2), where F(1) = 1 and F(2) = 1.
+//
+// It turns out that F(541), which contains 113 digits, is the first Fibonacci number
+// for which the last nine digits are 1-9 pandigital (contain all the digits 1 to 9,
+// but not necessarily in order). And F(2749), which contains 575 digits, is the first
+// Fibonacci number for which the first nine digits are 1-9 pandigital.
+//
+// Given that F(k) is the first Fibonacci number for which the first nine digits AND
+// the last nine digits are 1-9 pandigital, find k.
+//
+// Solution: 329468
+
+let fibonacciGenerator =
+    seq {
+        let mutable n0, n1 = 0I, 1I
+        while true do
+            yield n1
+            let temp = n1
+            n1 <- n0 + n1
+            n0 <- temp
+    }
+
+let isDoublyPandigital n =
+    let isLeadingPandigital (str : string) =
+        let leadingString = str.ToCharArray().[0..8] |> Array.sort |> System.String.Concat
+        leadingString = "123456789"
+    let isTrailingPandigital (str : string) =
+        str.ToCharArray() |> Array.rev |> System.String.Concat |> isLeadingPandigital
+    let str = n.ToString()
+    if str.Length < 9 then
+        false
+    else
+        isLeadingPandigital str && isTrailingPandigital str
+
+Seq.find isDoublyPandigital fibonacciGenerator |> printfn "%A"
