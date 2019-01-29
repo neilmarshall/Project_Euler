b29a1f87d9396e7dbbae50107697c068a75e0ccf
neilmarshall <neil.marshall@dunelm.org.uk>
2019-01-29 19:10:20 +0000

    fix (132): Update description of problem

--- 132/PE_132.cpp
+++ 132/PE_132.cpp
@@ -5,7 +5,7 @@ to be a repunit of length k.
 For example, R(10) = 1111111111 = 11×41×271×9091, and the sum of these prime
 factors is 9414.
 
-Find the sum of the first forty prime factors of R(109).
+Find the sum of the first forty prime factors of R(10^9).
 
 Solution: 843296
 */
