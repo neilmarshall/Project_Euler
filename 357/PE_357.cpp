be2a8940abfab50b26b3246bfcca78e044a8814a
neilmarshall <neil.marshall@dunelm.org.uk>
2019-03-13 19:32:01 +0000

    feat: Add solution to problem 357

--- 357/PE_357.cpp
+++ 357/PE_357.cpp
@@ -1,63 +1,58 @@
+/*
+Consider the divisors of 30: 1, 2, 3, 5, 6, 10, 15, 30.
+It can be seen that for every divisor d of 30, d + 30 / d is prime.
+
+Find the sum of all positive integers n not exceeding 100,000,000
+such that for every divisor d of n, d + n / d is prime.
+
+Solution: 1739023853137
+*/
+
 #include <cmath>
 
 #include <iostream>
-#include <set>
 #include <vector>
 
-class MemoizedPrimeChecker
-{
-public:
-	MemoizedPrimeChecker(long n) { known_primes = get_primes_up_to_n(n); }
-	bool is_prime(long n) { return known_primes.find(n) != known_primes.end(); }
+std::vector<bool> get_prime_flags_up_to_n(const long& n) {
 
-private:
+    if (n <= 1) { return std::vector<bool>(); }
 
-	std::set<long> known_primes;
+    std::vector<bool> flags = { false, false };
+    for (long p = 2; p <= n; p++) {
+        flags.push_back(true);
+    }
 
-	std::set<long> get_primes_up_to_n(const long& n) {
+    for (long p = 2; p <= n / 2; p++) {
+        if (flags[p]) {
+            for (long q = 2; q <= n / p; q++) {
+                flags[p * q] = false;
+            }
+        }
+    }
 
-		if (n <= 1) { return std::set<long>(); }
-
-		std::vector<bool> flags = { false, false };
-		for (long p = 2; p <= n; p++) {
-			flags.push_back(true);
-		}
-
-		for (long p = 2; p <= n / 2; p++) {
-			if (flags[p]) {
-				for (long q = 2; q <= n / p; q++) {
-					flags[p * q] = false;
-				}
-			}
-		}
-
-		std::set<long> primes;
-		for (long i = 2; i <= n; i++) {
-			if (flags[i]) { primes.insert(i); }
-		}
-
-		return primes;
-	}
-};
+    return flags;
+}
 
 int main()
 {
-	long limit = 1000000;
-	auto memoized_prime_checker = MemoizedPrimeChecker(limit + 1);
-	long total = 0;
+	long limit = 100000000;
+
+	auto prime_checker = get_prime_flags_up_to_n(limit + 1);
 
 	std::vector<bool> flags;
 	for (long i = 0; i <= limit; i++) { flags.push_back(true); }
+
 	for (long d = 1; d <= static_cast<long>(sqrt(limit)); d++) {
-		std::cout << d << '\n';
 		long i = 0;
 		do {
 			long index = d * d + i * d;
 			if (index > limit) { break; }
 			long residual = d + index / d;
-			if (!memoized_prime_checker.is_prime(residual)) { flags[index] = false; }
+			if (!prime_checker[residual]) { flags[index] = false; }
 		} while (++i);
 	}
+
+	long total = 0;
 	for (long i = 0; i <= limit; i++)
 		if (flags[i]) { total += i; }
 	std::cout << total << std::endl;
