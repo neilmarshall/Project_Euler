ab99b322620acb03236bec96e399fd2a714a95d3
neilmarshall <neil.marshall@dunelm.org.uk>
2019-06-13 13:13:49 +0100

    feat (123): Add solution to problem 123

---
+++ 123/PE_123.py
@@ -0,0 +1,33 @@
+#! venv/bin/python3.7
+"""
+Let p_n be the nth prime: 2, 3, 5, 7, 11, ..., and let r be the remainder
+when (p_n - 1)^n + (p_n + 1)^n is divided by p_n^2.
+
+For example, when n = 3, p_3 = 5, and 4^3 + 6^3 = 280 ≡ 5 mod 25.
+
+The least value of n for which the remainder first exceeds 10^9 is 7037.
+
+Find the least value of n for which the remainder first exceeds 10^10.
+
+Solution: 21035
+"""
+from pyutils.primes import get_primes_up_to_n
+
+def PE123(limit, prime_limit=2):
+    """
+    >>> PE123(1e9)
+    7037
+
+    >>> PE123(1e10)
+    21035
+    """
+    psr = lambda p, n: ((p - 1)**n + (p + 1)**n) % p**2
+    for n, p in enumerate(get_primes_up_to_n(prime_limit), 1):
+        remainder = psr(p, n)
+        if remainder >= limit:
+            return n
+    return PE123(limit, prime_limit * 2)
+
+
+if __name__ == '__main__':
+    import doctest; doctest.testmod(verbose=True)
